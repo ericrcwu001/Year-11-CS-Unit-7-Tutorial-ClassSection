0f9f8c382770f497237c4d866645710f508c5e3d
ericrcwu001 <ericrw2025@student.cis.edu.hk>
2023-02-22 13:11:29 +0800

    fixed junit4

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,37 @@
 public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int yearLevel;
+    private static int studentCount = 1;
+
+    public Student(String firstName, String lastName, int age, int yearLevel) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.yearLevel = yearLevel;
+        this.id = studentCount++;
+    }
+
+    public int getId() {return id;}
+    public int getAge() {return age;}
+    public String getLastName() {return lastName;}
+    public String getFirstName() {return firstName;}
+    public int getYearLevel() {return yearLevel;}
+    public int getStudentCount() {return studentCount;}
+
+    public void setAge(int age) {this.age = age;}
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+    public void setLastName(String lastName) {this.lastName = lastName;}
+    public void setYearLevel (int yearLevel) {this.yearLevel = yearLevel;}
+    public void setId(int id) {this.id = id;}
+
+    public boolean equals(Student student) {
+        if (firstName.compareTo(student.getFirstName()) == 0 &&
+                lastName.compareTo(student.getLastName()) == 0 && age == student.getAge() &&
+                yearLevel == student.getYearLevel()) return true;
+        return false;
+    }
+
 }
